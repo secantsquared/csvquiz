a789fd7d1b1bf703bdb3ac216f63ec57c02a8a8e
Ryan Boris <ryan.boris@gmail.com>
2021-03-18 00:00:09 -0400

    prototype out reading in and parsing csv

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("hi")
+	csvfile, err := os.Open("problems.csv")
+
+	if err != nil {
+		log.Fatalln("Could not open CSV file.")
+	}
+
+	r := csv.NewReader(csvfile)
+
+	for {
+		record, err := r.Read()
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("What is %s?\n", record[0])
+	}
 }
